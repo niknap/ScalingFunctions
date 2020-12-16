588b2b7199f189dffe6700d297c8ba892d961c7f
niknap <nikoknapp@posteo.de>
2020-12-16 15:33:40 +0100

    Revert accidental commit

--- R/arithcv_of_lognorm_function.R
+++ R/arithcv_of_lognorm_function.R
@@ -1,22 +1,3 @@
-# Copyright (C) 2019 Dr. Nikolai Knapp, UFZ
-#
-# This file is part of the ScalingFunctions R package.
-#
-# The ScalingFunctions R package is free software: you can redistribute
-# it and/or modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# ScalingFunctions is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with ScalingFunctions. If not, see <http://www.gnu.org/licenses/>.
-
-
-
 #' Arithmetic CV of lognormal distribution
 #'
 #' Calculation of the arithmetic coefficient of variation of a lognormal
